02106cd3a3fa17f71a5d4145880d4e93ecdee76f
Galagamage Umanda Thathsarani <guthathsarani@students.nsbm.lk>
2020-12-27 02:12:30 -0800

    Umanda

---
+++ Stock_Supplier_Display.sql
@@ -0,0 +1,10 @@
+create procedure Stock_Supplier_Display
+as
+begin
+select s.Item_Id,s.Item_Name,p.Supplier_ID
+from Stock_Table s
+ join Supplier_Table p
+ON
+s.Supplier_ID=p.Supplier_ID
+End;
+EXEC Stock_Supplier_Display
