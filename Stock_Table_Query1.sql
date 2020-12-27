cb35c02e8f7355ed398161d9b076d898e415a279
Galagamage Umanda Thathsarani <guthathsarani@students.nsbm.lk>
2020-12-27 02:08:32 -0800

    Umanda

---
+++ Stock_Table_Query1.sql
@@ -0,0 +1,25 @@
+--insert item details to the table--
+Insert into Stock_Table values(20001,'Blue Pen',20,400,186051);
+Insert into Stock_Table values(20002,'Black Pen',20,400,186051);
+Insert into Stock_Table values(20003,'Pencil',15,500,186051);
+Insert into Stock_Table values(20004,'Note Book',150,400,186051);
+Insert into Stock_Table values(20005,'A5 Book',80,300,186051);
+Insert into Stock_Table values(20006,'CR Book',200,260,186051);
+Insert into Stock_Table values(20007,'B5 Book',120,400,186051);
+Insert into Stock_Table values(20008,'pencil case',455,170,186051);
+
+--insert item details to the table--
+Insert into Stock_Table values(20010,'Blue Pen',20,300,186053);
+Insert into Stock_Table values(20011,'Black Pen',20,350,186053);
+Insert into Stock_Table values(20012,'Pencil',15,500,186053);
+Insert into Stock_Table values(20013,'Note Book',180,400,186053);
+Insert into Stock_Table values(20014,'A5 Book',90,300,186053);
+Insert into Stock_Table values(20015,'CR Book',210,260,186053);
+Insert into Stock_Table values(20016,'B5 Book',140,270,186053);
+Insert into Stock_Table values(20017,'pencil case',455,170,186053);
+Insert into Stock_Table values(20018,'Flie Cover',10,170,186053);
+
+
+
+
+select*from Stock_Table;
