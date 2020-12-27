95a630fac9493d903b4ec369fe8662662c0e64cc
Galagamage Umanda Thathsarani <guthathsarani@students.nsbm.lk>
2020-12-27 02:09:45 -0800

    Umanda

---
+++ Stock_Table_Query2.sql
@@ -0,0 +1,12 @@
+--insert item details to the table--
+Insert into Stock_Table values(50001,'Novel',670,100,186057);
+Insert into Stock_Table values(50002,'Teen Novel',720,350,186059);
+Insert into Stock_Table values(50003,'kids Story Book',300,500,186058);
+Insert into Stock_Table values(70001,'School Bag',1180,20,186064);
+Insert into Stock_Table values(70004,'Water Bottle',390,180,186065);
+Insert into Stock_Table values(70015,'Lunch Box',410,260,186066);
+Insert into Stock_Table values(90016,'Birthday Card',140,270,186066);
+Insert into Stock_Table values(90017,'Teachers Day Card',155,170,186066);
+Insert into Stock_Table values(90018,'New Year Card',110,170,186066);
+
+select*from Stock_Table;
