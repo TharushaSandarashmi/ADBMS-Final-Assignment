fb4b1186247e030a7a3c2d8469debf15a6ed324d
TharushaSandarashmi <tharushasandarashmi8@gmail.com>
2020-12-29 00:39:04 +0530

    Add files via upload

---
+++ Insert_Data_Supplier.sql
@@ -0,0 +1,21 @@
+use BookShopDb;
+
+insert into Supplier_Table(Supplier_Id,Supplier_Name,Supplier_ContactNo,Supplier_Address,Supply_Date) 
+values 
+
+(186050 ,  'Rathna'		 ,       0723353786		,'71/5,Meegahawatta'   , '2-Jan-21'	),
+(186051	,   'Richard'	,	        0773490986	,	'77/5,delgoda'		,   '4-Jan-21'	),
+(186052	 ,  'Promate'	,	        0775476786	,	'56/5,mattakuuliya'	 ,  '4-Jan-21'	),
+(186053	 , 'Atlas'		 ,           0723497486	,	'67/2,Makola'	,	   '5-Jan-21'	),
+(186054	 ,  'Sammanee'	,	        0703345786	,	'90/1,Mawaramandiya' ,  '4-Jan-21'	),
+(186055	 , 'Cial'		 ,           0772909678	,	'74/7,siyambalape'	  , '9-Jan-21'	),
+(186056	 ,  'Mulpiyali publishers',	0115316786	,	'72/6,udupila'		,   '11-Jan-21'  ),
+(186057	 ,  'Suriya publishers'	,	0114566786	,	'79/5,katukurunda'   ,  '11-Jan-21'  ),
+(186058	 ,  'Tharanga publishers',	0750006786	,	'81/7,biyamaga'	,	   '12-Jan-21'  ),
+(186059	 , 'Sarasavi publishers',	0766216786	,	'85/3,kaduwela'	,	   '12-Jan-21'  ),
+(186062	 ,  'Hutch'		        ,    0783468786	,	'87/9,maradana'	,	   '15-Jan-21'  ),
+(186064	 , 'A.M.Perera'		  ,  0772963786		,'30/5,anuradhapura',	   '8-Jan-21' 	),
+(186065	 ,  'A.V.G.Silva'	,	    0700066786	,	'37/2,kadawatha',	   '8-Jan-21'	),
+(186066	 ,  'K.A.Jayasooriya',		0722156786	,	'49/1,gampaha'	,	   '8-Jan-21'	),
+(186070	 ,  'Schoolmate'		,    0767216786	,	'84/5,kaduwela'	,	   '4-Jan-21'	)
+
