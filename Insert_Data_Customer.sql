beadf92cdd35df0ab3b862b5f1bf2d118c20dee4
TharushaSandarashmi <tharushasandarashmi8@gmail.com>
2020-12-30 00:15:20 +0530

    Add files via upload

---
+++ Insert_Data_Customer.sql
@@ -0,0 +1,22 @@
+use BookShopDb;
+
+insert into Customer_Table (Customer_Id,Customer_Name,Quantity,Customer_Address,TelNo,Service_Id)
+
+values
+
+(902155	,	'A.V.D.Silva'		,    2	,	'87/2,kaduwela'		,    0751230981	,	3	),
+(902156	,	'H.K.D.Wanigasuriya',	1	,	'98,1,kadawatha'	,	0770987652	,	9	),
+(902165	,	'L.U.W.Kalupahana'	,	6	,	'87/5,awissawella'	,	0757654323	,	5	),
+(914026	,	'A.Samanalee Kumari',	10	,	'77/5,mattakkuliya'	,	0764561094	,	10	),
+(914028	,	'Y.T.Mahanama'		,   8	,	'77/3,mattakkuliya'	,	0714328765	,	7	),
+(902167	,	'P.G.Kariyawasam'	,	2	,	'90/4,mahara'		,   0761095436	,	10	),
+(902168	,	'S.F.K.Wijesinghe'	,	4	,	'48/5,makola'		,    0777892107	,	3	),
+(902170	,	'S.Malindu Jayasooriya',10	,	'78/3,kuliyapitiya'	,	0728769988	,	4	),
+(854621	,	'L.Nnavidu Thineth'	,	4	,	'67/1,meegahawatta'	,	0728769989	,	7	),
+(854667	,	'D.U.Rathwatte'		 ,   1	,	'34/5,siyambalape'	,	0728769980	,	1	),
+(914032	,	'K.A.D.Pushpalatha',		9	,	'56/4,biyagama'	,   0728769981	,	2	),
+(914033	,	'M.K.V.Manel'		,    7	,	'66/3,nuwaraeliya'	,	0728769982	,	6	),
+(854670	,	'U.J.Sakuni Ama'	,	4	,	'43/2,kandy'	,	    0728769983	,	1	),
+(902158	,	'E.G.Dewruwan Perera',	6	,	'14/5,maradana'	,	    0728769985	,	7	),
+(902181	,	'K.L.P.Pathirana'	,	5	,	'46/7,kuruduwatta',		0728769986	,	3	)
+
