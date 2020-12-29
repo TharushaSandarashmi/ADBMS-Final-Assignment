e1e0969bb0bee82fa8c9e42ea003d1f862ba0ccb
TharushaSandarashmi <tharushasandarashmi8@gmail.com>
2020-12-30 00:25:25 +0530

    Add files via upload

---
+++ Udate_Supplier_Table_t.sql
@@ -0,0 +1,19 @@
+create procedure updateSupplierTableData(
+@Supplier_Id int,
+@Supplier_Name varchar(40),
+@Supplier_ContactNo int,
+@Supplier_Address varchar(50),
+@Supply_Date varchar(30)
+)
+AS
+BEGIN
+
+update  Supplier_Table set @Supplier_Name=@Supplier_Name,@Supplier_ContactNo=@Supplier_ContactNo,
+Supplier_Address=@Supplier_Address,Supply_Date=@Supply_Date
+
+where
+Supplier_Id=@Supplier_Id;
+
+END
+
+
