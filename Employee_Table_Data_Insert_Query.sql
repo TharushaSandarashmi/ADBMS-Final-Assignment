04e5f59b1594e0a80111358f7b43688fb4a1bc6d
Limasha Aswini <60962997+limasha@users.noreply.github.com>
2020-12-30 15:47:43 +0530

    Limasha

---
+++ Employee_Table_Data_Insert_Query.sql
@@ -0,0 +1,23 @@
+
+
+EXEC employeeDetails '1','Kirihenage Lakshman Warnasiri','0765253319','223 B2, Millaniya,Horana','1974-08-21','Male','Owner','0';
+
+EXEC employeeDetails '2','Kirihenage Dikson Thilaksiri','0756187782','172 Samagi mawatha, Pitipana, Homagama','1981-10-25','Male','Manager','38000';
+
+EXEC employeeDetails '3','Dona Dilshani Kavindya Samaraweera','0713388241','82/4, Udugoda, Bandaragama','1983-02-05','Female','Accountant','35000';
+
+EXEC employeeDetails '4','Kankanamge Tharindu Deshan Perera','0776517883','32 A1, Manamulla, Panadura','1981-09-11','Male','Accountant','35000';
+
+EXEC employeeDetails '5','Biyanka Prasadi','0756511823','129 Pasal mawatha, Sawgas handiya, Horana','1989-07-09','Female','Employee','27000';
+
+EXEC employeeDetails '6','Sahan Kawshalya Perera','0726991321','28/5, Yatiyana, Bandaragama','1991-04-23','Male','Employee','22000';
+
+EXEC employeeDetails '7','Sampath Tharaka Weerasingha','0772381129','28/5, Alubomulla, Panadura','1994-12-20','Male','Service Supplier','20000';
+
+EXEC employeeDetails '8','Udani Hansini Fernando','0715516139','289/A, Pannipitiya, Homagama','1987-11-02','Female','Employee','22000';
+
+EXEC employeeDetails '9','Gallage Pasan Kumara','0767327149','442/B5, Madamulla, Maharagama','1986-05-30','Male','Service Supplier','24000';
+
+
+
+
