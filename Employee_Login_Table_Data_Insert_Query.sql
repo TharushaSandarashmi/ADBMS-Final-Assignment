d4e30453e3ddbf7dfbd94ec9a83b34816898bce9
Limasha Aswini <60962997+limasha@users.noreply.github.com>
2020-12-30 15:47:11 +0530

    Limasha

---
+++ Employee_Login_Table_Data_Insert_Query.sql
@@ -0,0 +1,7 @@
+
+
+EXEC employeeLoginDetails 'L001','Lakshman','Lakshman001','1';
+EXEC employeeLoginDetails 'L002','Dikson','Dikson002','2';
+EXEC employeeLoginDetails 'L003','Biyanka','Biyanka003','5';
+			
+			
