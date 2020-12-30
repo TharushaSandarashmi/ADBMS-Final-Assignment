0e6e53185810816e8ef30fa4c1208c75e691800f
TharushaSandarashmi <tharushasandarashmi8@gmail.com>
2020-12-31 02:07:31 +0530

    Add files via upload

---
+++ Display_Service_Data.sql
@@ -0,0 +1,15 @@
+drop procedure if exists c_Customer_Table;
+go
+
+create procedure c_Customer_Table(@Service_Id INT)
+
+AS 
+BEGIN
+
+select Customer_Id,Customer_Name,Quantity from Customer_Table
+where Service_Id=@Service_Id;
+
+END;
+
+EXEC c_Customer_Table @Service_Id='007';
+
