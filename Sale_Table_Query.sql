f43c47653dedb193ab06fe12ca17224e07b0c71c
Galagamage Umanda Thathsarani <guthathsarani@students.nsbm.lk>
2020-12-30 22:16:11 -0800

    Umanda

---
+++ Sale_Table_Query.sql
@@ -0,0 +1,27 @@
+Insert into Sale_Table values(4001, '2020-12-23', 20002, 20, 1, 2);
+Insert into  Sale_Table values(4002, '2020-12-23',20001,20, 4,2);
+Insert into Sale_Table values(4003, '2020-12-23',20003,15, 2,2);
+Insert into Sale_Table values(4004, '2020-12-23',20004,150, 1,2);
+
+
+
+Insert into Sale_Table values(4005, '2020-11-03',20005,80,1,1);
+Insert into Sale_Table values(4006, '2020-12-20',20006,200,1,3);
+Insert into Sale_Table values(4007, '2020-10-06',20007,120,1,5);
+Insert into Sale_Table values(4008, '2020-12-13',20008,455,1,6);
+
+
+
+Insert into Sale_Table values(4009, '2020-10-05',50001,670,1,1);
+Insert into Sale_Table values(4010, '2020-11-13',50002,720,3,1);
+Insert into Sale_Table values(4011, '2020-10-23',50003,300,2,8);
+Insert into Sale_Table values(4012, '2020-02-09',70001,1180,1,7);
+
+
+
+Insert into Sale_Table values(4013, '2020-12-11',70004,390,1,1);
+Insert into Sale_Table values(4014, '2020-12-10',70015,410,1,5);
+Insert into Sale_Table values(4015, '2020-10-15',90016,140,1,2);
+
+
+Select* from Sale_Table;
