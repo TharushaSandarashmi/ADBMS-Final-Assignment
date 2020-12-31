7de404b59be9c55ae6af3b9537763d4e98dda08f
Galagamage Umanda Thathsarani <guthathsarani@students.nsbm.lk>
2020-12-30 22:15:06 -0800

    Umanda

---
+++ Sale_Table.sql
@@ -0,0 +1,11 @@
+create table Sale_Table(
+Bill_NO int NOT NULL,
+Sale_Date date NOT NULL,
+Item_ID int NOT NULL,
+Item_Sale_Price float NOT NULL,
+Item_Sale_Qty int NOT NULL,
+Employee_ID int NOT NULL,
+primary key(Bill_NO)
+);
+
+Select* from Sale_Table;
