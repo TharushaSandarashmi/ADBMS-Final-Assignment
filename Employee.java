bab9663cc16d55836844334262fb26c510361f6d
Limasha Aswini <60962997+limasha@users.noreply.github.com>
2021-01-06 10:41:12 +0530

    Limasha

---
+++ Employee.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Employee {
+    
+    private int id;
+    private String name,contactno,address,dob,gender,designation,salary;
+    
+    public Employee(int id,String name,String contactno,String address,String dob,String gender,String designation,String salary)
+    {
+        this.id=id;
+        this.name=name;
+        this.contactno=contactno;
+        this.address=address;
+        this.dob=dob;
+        this.gender=gender;
+        this.designation=designation;
+        this.salary=salary;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the contactno
+     */
+    public String getContactno() {
+        return contactno;
+    }
+
+    /**
+     * @param contactno the contactno to set
+     */
+    public void setContactno(String contactno) {
+        this.contactno = contactno;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the dob
+     */
+    public String getDob() {
+        return dob;
+    }
+
+    /**
+     * @param dob the dob to set
+     */
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    /**
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @return the designation
+     */
+    public String getDesignation() {
+        return designation;
+    }
+
+    /**
+     * @param designation the designation to set
+     */
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    /**
+     * @return the salary
+     */
+    public String getSalary() {
+        return salary;
+    }
+
+    /**
+     * @param salary the salary to set
+     */
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+    
+    
+}
