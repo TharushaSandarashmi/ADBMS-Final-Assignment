da5e81e2d399352d545c969d8754588033edae97
Limasha Aswini <60962997+limasha@users.noreply.github.com>
2021-01-11 18:33:43 +0530

    Limasha

---
+++ deleteEmployeeLoginDetailsTrigger.sql
@@ -0,0 +1,8 @@
+
+CREATE TRIGGER deleteEmployeeLoginDetails
+ON Employee_Table
+After DELETE 
+AS
+BEGIN
+delete from Employee_Login_Table where NOT EXISTS(select Employee_ID from Employee_Login_Table);
+END;
