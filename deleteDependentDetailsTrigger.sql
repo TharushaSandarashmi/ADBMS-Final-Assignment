b94e8e7405aa73e31a47bafc6e664b353fb6ef6d
Limasha Aswini <60962997+limasha@users.noreply.github.com>
2021-01-15 08:29:14 +0530

    Limasha

--- deleteDependentDetailsTrigger.sql
+++ deleteDependentDetailsTrigger.sql
@@ -3,5 +3,5 @@ ON Employee_Table
 After DELETE 
 AS
 BEGIN
-delete from Dependent_Table where NOT EXISTS(select Employee_ID from Employee_Login_Table);
+delete from Dependent_Table where NOT EXISTS(select Employee_ID from Employee_Table);
 END;
